213f1ab961f47f9e4932366c985b7a4b2899dde9
Sujith Pasupuleti <SP15984@fulbright.com>
2026-02-05 11:42:04 -0600

    Initial commit

---
+++ myFastMCPServer.py
@@ -0,0 +1,17 @@
+from fastmcp import FastMCP
+
+mcp = FastMCP("My MCP Server")
+
+@mcp.tool
+def greet(name: str) -> str:
+    return f"Hello, {name}!"
+
+
+@mcp.tool
+def add(a: int, b: int) -> int:
+    """Adds two integer numbers together."""
+    return a + b
+
+
+if __name__ == "__main__":
+    mcp.run(transport="http", port=8000)
