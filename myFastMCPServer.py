bcc5fd6b6986ecf7fc20c991bb4b5c427f6ae99c
Sujith Pasupuleti <SP15984@fulbright.com>
2026-02-05 12:24:36 -0600

    Added changes

--- myFastMCPServer.py
+++ myFastMCPServer.py
@@ -1,29 +1,16 @@
-<<<<<<< HEAD
 from fastmcp import FastMCP
 
-mcp = FastMCP("My MCP Server")
+mcp = FastMCP(name="My MCP Server")
 
 @mcp.tool
 def greet(name: str) -> str:
+    """Greets the user by name."""
     return f"Hello, {name}!"
-
 
 @mcp.tool
 def add(a: int, b: int) -> int:
     """Adds two integer numbers together."""
     return a + b
 
-
 if __name__ == "__main__":
-=======
-from fastmcp import FastMCP
-
-mcp = FastMCP("My MCP Server")
-
-@mcp.tool
-def greet(name: str) -> str:
-    return f"Hello, {name}!"
-
-if __name__ == "__main__":
->>>>>>> e74f70099d643ad8290f59fa2c1e7ccd8ba9d65d
     mcp.run(transport="http", port=8000)
